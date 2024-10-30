0e533b0ff74d3392d0fb494efd1c7b5867267987
avantikasrivastav <105983351+avantika2195@users.noreply.github.com>
2024-10-31 00:43:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-<<<<<<< HEAD
+
 // Vite configuration with React and dependency optimization
 export default defineConfig({
   plugins: [react()],
@@ -12,13 +12,13 @@ export default defineConfig({
     alias: {
       // Optional: Define aliases if you want to use shorter paths in imports
       '@': '/src',
-=======
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       external: ['@emailjs/browser', 'react-hot-toast'],
->>>>>>> origin/main
+      origin/main
     },
   },
 });
