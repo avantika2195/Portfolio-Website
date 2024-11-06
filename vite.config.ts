39f62acb02d2af0c36644b447d11e68b64fa1699
avantikasrivastav <105983351+avantika2195@users.noreply.github.com>
2024-11-06 16:49:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'; // Ensure this is imported
 
 export default defineConfig({
-  base: '/Portfolio-Website/', // Update with your actual repo name if needed
+  base: '/', // Update with your actual repo name if needed
   plugins: [react()],
   optimizeDeps: {
     include: ["@emailjs/browser", "react-hot-toast"], // Pre-bundle dependencies
