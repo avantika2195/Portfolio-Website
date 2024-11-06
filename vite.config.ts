673c1e7c07a019e26f245464862ae2cf4e358129
Avantika Srivastava <srivastavaavantika465@gmail.com>
2024-11-06 16:45:31 +0530

    Fix merge conflict in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'; // Ensure this is imported
 
 export default defineConfig({
   base: '/Portfolio-Website/', // Update with your actual repo name if needed
-=======
-export default defineConfig({
-  base: '/', // This will serve the app from the root of the domain
->>>>>>> 001cd66ed95b99094814f8c1771cfe28f60dc439
   plugins: [react()],
   optimizeDeps: {
     include: ["@emailjs/browser", "react-hot-toast"], // Pre-bundle dependencies
